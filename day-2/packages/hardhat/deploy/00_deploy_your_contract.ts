f611f5c5fe9d59323ff7965865d95e2f0c09b918
Nicolas Tourne <nicotourne@gmail.com>
2024-07-21 09:41:23 -0300

    feat: Add MyToken and tests

--- day-2/packages/hardhat/deploy/00_deploy_your_contract.ts
+++ day-2/packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -8,7 +8,7 @@ import { Contract } from "ethers";
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployMyToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -22,10 +22,10 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("YourContract", {
+  await deploy("MyToken", {
     from: deployer,
     // Contract constructor arguments
-    args: [deployer],
+    args: ["Tandil", "TNDL", 18, "100000000000000000000"],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
@@ -33,12 +33,12 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   });
 
   // Get the deployed contract to interact with it after deploying.
-  const yourContract = await hre.ethers.getContract<Contract>("YourContract", deployer);
-  console.log("👋 Initial greeting:", await yourContract.greeting());
+  const myToken = await hre.ethers.getContract<Contract>("MyToken", deployer);
+  console.log("👋 Token symbol:", await myToken.symbol());
 };
 
-export default deployYourContract;
+export default deployMyToken;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployMyToken.tags = ["MyToken"];
